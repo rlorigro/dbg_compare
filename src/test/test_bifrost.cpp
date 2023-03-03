c1c2da4f7ea177641b48ca259491034b3390f9c4
Ryan <rlorigro@ucsc.edu>
2023-03-03 15:47:49 -0800

    Begin linking cuttlefish (WIP)

--- src/test/test_bifrost.cpp
+++ src/test/test_bifrost.cpp
@@ -1,6 +1,6 @@
 #include "bifrost/include/bifrost/CompactedDBG.hpp"
 
-// Taken from Bifrost documentation:
+// Adapted from Bifrost documentation:
 // https://github.com/pmelsted/bifrost/blob/master/doc/tutorial/Intro.md#creating-a-graph
 
 int main(){
@@ -10,23 +10,22 @@ int main(){
 
     cout << "K-mer size is " << cdbg.getK() << endl;
 
-    const string seq = "ACGTCGTACGTCCCGTAAACGTTAAACGTAAACGTGTGTGCAAAATGTCTAGTTTTTTTACGCTGATATAGTC";
+    const string reference_seq = "ACGTCGTACGTCCCGTAAACGTTAAACGTAAACGTGTGTGCAAAATGTCTAGTTTTTTTACGCTGATATAGTC";
+    const string query_sequence = "ACGTCGTACGTCCCGTAAACGTTAAACGTAAACGTGTGTGCAAAATGTCTAGTTTTTTTACGCTGATATAGTC";
 
-    cdbg.add(seq);
+    cdbg.add(reference_seq);
 
-    const string kmer_sequence = "ACGTCGTACGTCCCGTAAACGTTAAACGTAA";
-    const Kmer km = Kmer(kmer_sequence.c_str());
+    for (KmerIterator it_km(query_sequence.c_str()), it_km_end; it_km != it_km_end; ++it_km) { //non-ACGT char. are discarded
+        UnitigMap<> um = cdbg.find(it_km->first);
 
-    UnitigMap<> um = cdbg.find(km);
+        if (um.isEmpty) cout << "Kmer " << it_km->first.toString() << " was not found" << endl;
+        else {
+            const string unitig = um.referenceUnitigToString();
+            const string strandness = um.strand ? "forward" : "reverse-complement";
+            const size_t position = um.dist;
 
-    if (um.isEmpty) cout << "Kmer " << kmer_sequence << " was not found" << endl;
-    else {
-
-        const string unitig = um.referenceUnitigToString();
-        const string strandness = um.strand ? "forward" : "reverse-complement";
-        const size_t position = um.dist;
-
-        cout << "Kmer " << kmer_sequence << " was found in the " << strandness << " direction of unitig " << unitig << "at position " << position << '\n';
+            cout << "Kmer " << it_km->first.toString() << " was found in the " << strandness << " direction of unitig " << unitig << "at position " << position << '\n';
+        }
     }
 
     return 0;
