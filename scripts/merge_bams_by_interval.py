52ca34bf4b17a3dda685b6881db7c1c53c4a50a6
Ryan <rlorigro@ucsc.edu>
2023-03-17 15:17:03 -0700

    Better error handling

--- scripts/merge_bams_by_interval.py
+++ scripts/merge_bams_by_interval.py
@@ -9,27 +9,30 @@ import re
 
 
 def merge_bams_by_interval(bam_paths, contig, start, stop, output_directory):
-    region_string = "%s:%d-%d" % (contig, start, stop)
-    output_filename = region_string.replace(":","_") + ".fasta"
-    output_path = os.path.join(output_directory,output_filename)
-
     # Not attempting to share a token across threads, so each job gets a new instance
     token = GoogleToken()
 
     for path in bam_paths:
-        get_remote_region_as_fasta(
+        result = get_remote_region_as_fasta(
             bam_path=path,
             contig=contig,
             start=start,
             stop=stop,
-            output_path=output_path,
+            output_directory=output_directory,
             token=token
         )
 
+        if not result[0]:
+            return result
+
+    return True, None
+
 
 # Requires samtools installed!
-def get_remote_region_as_fasta(bam_path, contig, start, stop, output_path, token):
+def get_remote_region_as_fasta(bam_path, contig, start, stop, output_directory, token):
     region_string = "%s:%d-%d" % (contig, start, stop)
+    output_filename = region_string.replace(":","_") + ".fasta"
+    output_path = os.path.join(output_directory,output_filename)
 
     print(region_string)
 
@@ -42,11 +45,16 @@ def get_remote_region_as_fasta(bam_path, contig, start, stop, output_path, token
     with open(output_path, 'a') as file:
         sys.stderr.write(" ".join(samtools_view_args)+'\n')
 
-        p1 = subprocess.Popen(samtools_view_args, stdout=subprocess.PIPE)
-        p2 = subprocess.Popen(samtools_fasta_args, stdin=p1.stdout, stdout=file)
+        p1 = subprocess.Popen(samtools_view_args, stdout=subprocess.PIPE, cwd=output_directory)
+        p2 = subprocess.Popen(samtools_fasta_args, stdin=p1.stdout, stdout=file, cwd=output_directory)
         p2.communicate()
 
-    return output_path
+    success = (p2.returncode == 0)
+
+    if not success:
+        sys.stderr.flush()
+
+    return success, bam_path
 
 
 def main(bam_paths, bed_path, output_directory, n_cores):
@@ -65,7 +73,17 @@ def main(bam_paths, bed_path, output_directory, n_cores):
             args.append([bam_paths, contig, start, stop, output_directory])
 
     with Pool(processes=n_cores) as pool:
-        pool.starmap(merge_bams_by_interval, args)
+        successes = pool.starmap(merge_bams_by_interval, args)
+
+    all_succeeded = True
+    for s,success in enumerate(successes):
+        if not success[0]:
+            sys.stderr.write("ERROR: job for region %s:%d-%d failed\n" % (args[s][1], args[s][2], args[s][3]))
+            sys.stderr.write("\t%s\n" % (success[1]))
+            all_succeeded = False
+
+    if not all_succeeded:
+        exit(1)
 
 
 def parse_comma_separated_string(s):
